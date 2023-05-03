0750a2f46c2e2a8de36e3afd2e4cac1a9ff0fdc4
Anderson Souza <andersonsouza@zohomail.com>
2023-05-03 13:37:09 -0300

    adicionado o recurso de edição da categoria

--- categorias/CategoriasController.js
+++ categorias/CategoriasController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const CategoriasModel = require('./categorias');
 const slugify = require('slugify');
+const Categorias = require('./categorias');
 
 router.get('/admin/categorias', (req, res) => {
     
@@ -18,11 +19,32 @@ router.get('/admin/categorias', (req, res) => {
 
 });
 
+//tela de cadastro
 router.get('/admin/categorias/nova', (req, res) => {
     res.render('admin/categorias/form-cadastro');
 });
 
+//tela edição
+router.get('/admin/categoria/editar/:id?', (req, res) => {
 
+    var id = req.params.id ? req.params.id : false
+    
+    if(!id || isNaN(id)){
+        res.redirect('/admin/categorias');
+    }else{
+        
+        CategoriasModel.findByPk(id).then(categoria => {
+            res.render('admin/categorias/form-edicao', {
+                categoria: categoria
+            });
+        }).catch(erro => {
+            res.redirect('/admin/categorias');
+        });
+    }
+
+});
+
+//ação de insert no banco de dados
 router.post('/admin/categorias/salvar', (req, res) => {
 
     var title = req.body.title;
@@ -41,6 +63,29 @@ router.post('/admin/categorias/salvar', (req, res) => {
     }
 });
 
+//ação de update no banco de dados
+router.post('/admin/categoria/editar', (req, res) => {
+    
+    var id = req.body.id;
+    var title = req.body.title;
+
+    if(isNaN(id) || title == undefined){
+       res.redirect('/admin/catgoria/editar');
+    }else{
+
+        CategoriasModel.update({title: title},{
+            where: {
+                id: id
+            }
+        }).then(() => {
+            res.redirect('/admin/categorias');
+        });
+
+    }    
+
+});
+
+//ação de delete no banco de dados
 router.get('/admin/categoria/excluir/:id?', (req, res) => {
 
     var id = req.params.id ? req.params.id : false
